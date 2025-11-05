cf846fb7a321d21cf991d585f1a58680987a6a5f
BernardoYuji <hanashirobernardo@gmail.com>
2025-11-04 23:29:04 -0300

    page missed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { RevealOnScroll } from "../components/RevealOnScroll";
+
+export const Home = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
+      <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-tight">
+            Hi, I'm Bernardo Yuji!
+            <br />
+            A Software Engineer student
+          </h1>
+
+          <p className="text-white-400 text-lg mb-8 max-w-lg mx-auto">
+            I'm a Full-Stack developer who loves to code, build applications and resolve problems.
+            <br />
+            My goal is to build solutions that can really make a difference.
+          </p>
+
+          <div className="flex justify-center space-x-4">
+            <Link
+              to="/projects"
+              className="bg-blue-600 text-white py-3 px-6 rounded font-medium transition
+                relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            >
+              View Projects
+            </Link>
+
+            <Link
+              to="/contact"
+              className="border border-blue-500/50 text-blue-600 py-3 px-6 rounded font-medium transition-all duration-200
+                hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+            >
+              Contact Me
+            </Link>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
