b9bfb86f8cb5f7557a8f4adbd31fb70e9bf9294b
BernardoYuji <hanashirobernardo@gmail.com>
2025-11-04 23:28:40 -0300

    project src

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { NavBar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { Footer } from "./components/Footer"
+import "./index.css";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-grey-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+        <Outlet />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Projects } from "./pages/Projects";
+import { ProjectDetail } from "./components/ProjectsDetails"
+import { Contact } from "./pages/Contact";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "projects", element: <Projects /> },
+      { path: "projects/:id", element: <ProjectDetail /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+]);
+
+export const AppRouter = () => <RouterProvider router={router} />;
