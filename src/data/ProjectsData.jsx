181f7006c0d8a778b105b553fcd5651a233ad321
BernardoYuji <hanashirobernardo@gmail.com>
2025-11-04 23:28:52 -0300

    project data

---
+++ src/data/ProjectsData.jsx
@@ -0,0 +1,55 @@
+export const projectsData = [
+  {
+    id: "vinheria",
+    title: "CheckPoint Vinheria",
+    description:
+      "Project built for a wine store called Vinheria Agnello. It’s an e-commerce platform focused on delivering a great user experience (UX).",
+    tech: ["HTML", "CSS"],
+    github: "https://github.com/thiagolima-tech/CheckpointVinheria",
+  },
+  {
+    id: "cp3-webdev",
+    title: "CP3 - WebDev",
+    description: "Project built for a college work.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/BernardoYuji/CP3-Web-Dev",
+  },
+  {
+    id: "gs-front-end",
+    title: "GS_Front-End",
+    description:
+      "Project built for the semestral college test. Me and my classmate made a LandingPage to explain how our solution can help the society.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/BernardoYuji/GS_Front-End",
+  },
+  {
+    id: "crud",
+    title: "CP_CRUD",
+    description:
+      "Project built for a college work that was about to create a CRUD using JSON.",
+    tech: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/BernardoYuji/cp_CRUD",
+  },
+  {
+    id: "python",
+    title: "Sprint_3_Python",
+    description:
+      "Project built for a company called Passa a Bola. I made a program that can organize a championship.",
+    tech: ["Python"],
+    github: "https://github.com/BernardoYuji/sprint_3_python",
+  },
+  {
+    id: "gs-edge",
+    title: "GS-Edge-Computing",
+    description:
+      "Project built for the semestral test. Made a Flood Monitoring System.",
+    tech: [
+      "Arduino Uno",
+      "Ultrasonic Sensor",
+      "Presence Sensor",
+      "LCD Screen",
+      "LED",
+    ],
+    github: "https://github.com/Arriatea/GsEdge_Computing",
+  },
+];
